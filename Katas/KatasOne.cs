f83bde05edfbc34d95d66f4c15010de62e1c5b0e
Thomas <thomashj93@gmail.com>
2023-03-27 20:38:10 +0200

    ready up KatasOne

---
+++ Katas/KatasOne.cs
@@ -0,0 +1,11 @@
+namespace Katas;
+
+public class KatasOne
+{
+    public string Message { get; init; }
+    
+    public KatasOne(string message)
+    {
+        Message = message;
+    }
+}
--- Katas/Program.cs
+++ Katas/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using Katas;
+
+var katas = new KatasOne("KatasOne");
+
 Console.WriteLine("Katas Go!"); 
+Console.WriteLine("-----------------------");
+Console.WriteLine(katas.Message);
