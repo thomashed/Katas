9c69250cf955b7bbc6151e2c19f11468d1847852
Thomas <thomashj93@gmail.com>
2023-04-14 07:46:23 +0200

    add helper comments until imlementation

--- Katas/KatasOne.cs
+++ Katas/KatasOne.cs
@@ -88,6 +88,14 @@ public class KatasOne
         return (long)Math.Pow(n, 3);
     }
     
+    // Testing: [0, 0, 0, 1] ==> 1
+    // Testing: [0, 0, 1, 0] ==> 2
+    // Testing: [0, 1, 0, 1] ==> 5
+    // Testing: [1, 0, 0, 1] ==> 9
+    // Testing: [0, 0, 1, 0] ==> 2
+    // Testing: [0, 1, 1, 0] ==> 6
+    // Testing: [1, 1, 1, 1] ==> 15
+    // Testing: [1, 0, 1, 1] ==> 11
     public static int binaryArrayToNumber(int[] BinaryArray)
     {
         throw new NotImplementedException();
