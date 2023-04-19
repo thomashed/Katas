a1e2c5fd67449e096bbbd089e41c69972d62096c
Thomas <thomashj93@gmail.com>
2023-04-19 07:26:28 +0200

    revision SumTwoSmalletNumbers to use Linq

--- Katas/KatasOne.cs
+++ Katas/KatasOne.cs
@@ -131,26 +131,7 @@ public class KatasOne
         return word ? "Yes" : "No";
     }
 
-    public static int SumTwoSmallestNumbers(int[] numbers)
-    {
-        var lowest1 = numbers[0];
-        var lowest2 = numbers[1];
-
-        foreach (var number in numbers)
-        {
-            if (number < lowest1)
-            {
-                lowest1 = number;
-            }
-
-            if (number < lowest2 && number != lowest1)
-            {
-                lowest2 = number;
-            }
-        }
-
-        return lowest1 + lowest2;
-    }
+    public static int SumTwoSmallestNumbers(int[] numbers) => numbers.OrderBy(i => i).Take(2).Sum();
     
-        
+    
 }
