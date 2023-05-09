af354631c5ec214d920527a7a68ea0ab63c6fdd6
Thomas <thomashj93@gmail.com>
2023-05-09 09:10:08 +0200

    add sum v2

--- Katas/KatasOne.cs
+++ Katas/KatasOne.cs
@@ -184,4 +184,9 @@ public class KatasOne
         return string.Concat(s.Select(c => char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)));
     }
     
+    public static int SumTwoSmallestNumbers2(int[] numbers) => numbers.Order().Take(2).Sum();
+    
+    
+    
+    
 }
