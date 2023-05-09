5d4be978225b22279c86d52b690c46dc674978d6
Thomas <thomashj93@gmail.com>
2023-05-09 09:14:44 +0200

    ammend OrderBy

--- Katas/KatasOne.cs
+++ Katas/KatasOne.cs
@@ -184,7 +184,7 @@ public class KatasOne
         return string.Concat(s.Select(c => char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)));
     }
     
-    public static int SumTwoSmallestNumbers2(int[] numbers) => numbers.Order().Take(2).Sum();
+    public static int SumTwoSmallestNumbers2(int[] numbers) => numbers.OrderBy(i => i).Take(2).Sum();
     
     
     
