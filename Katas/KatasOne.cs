12b5910dd5e5d508cff5fae08d935a4248e4c1b7
Thomas <thomashj93@gmail.com>
2023-05-28 08:41:30 +0200

    add IsOpposite, v3

--- Katas/KatasOne.cs
+++ Katas/KatasOne.cs
@@ -267,13 +267,10 @@ public class KatasOne
         listOfItems.OfType<int>();
 
     // "ab","AB"     -> true
-    public static bool IsOpposite3(string s1, string s2)
-    {
-        var isOpposite = !string.IsNullOrEmpty(s1) 
+    public static bool IsOpposite3(string s1, string s2) => !string.IsNullOrEmpty(s1) 
                          && !string.IsNullOrEmpty(s2) 
                          && s1.SequenceEqual(s2.Select(c => char.IsLower(c) ? char.ToUpper(c) : char.ToLower(c)));
-        return isOpposite;
-    }
+    
 
 }
 
