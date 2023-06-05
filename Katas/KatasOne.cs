102f21af46f57782ff9f5ec1151a0fbd083184ff
Thomas <thomashj93@gmail.com>
2023-06-05 07:33:08 +0200

    add alphaNumeric method check, v5

--- Katas/KatasOne.cs
+++ Katas/KatasOne.cs
@@ -297,7 +297,8 @@ public class KatasOne
     
     public static bool Alphanumeric2(string str)
     {
-        return !string.IsNullOrEmpty(str) && Regex.IsMatch(str, "^[a-zA-Z0-9]*$");
+        var isAlphanumeric = !string.IsNullOrEmpty(str) && str.All(c => char.IsLetterOrDigit(c));
+        return isAlphanumeric;
     }
     
 }
