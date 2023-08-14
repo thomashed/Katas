6e87383f183e0fc4795630563403190ca1414d5f
Thomas <thomashj93@gmail.com>
2023-08-14 06:51:45 +0200

    format

--- Katas/KatasOne.cs
+++ Katas/KatasOne.cs
@@ -696,9 +696,29 @@ public class KatasOne
         return Int32.Parse(orderedNumbers);
     }
     
+    // 2,2,2 --> 2
+    // 2,6,2 --> 12 (2 + 4 + 6)
+    // 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+    // 1,5,3  --> 5 (1 + 4)
+    
+    // Assert.AreEqual(12, Kata.SequenceSum(2, 6, 2));
+    
+    // If start value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
     public static int SequenceSum(int start, int end, int step)
     {
-        throw new NotImplementedException();
+        var sequenceSum = start;
+        
+        if (start > end)
+        {
+            return 0;
+        }
+
+        for (int i = 0; i < end; i++)
+        {
+            sequenceSum += step + sequenceSum;
+        }
+        
+        return sequenceSum == end ? sequenceSum + end : sequenceSum;
     }    
     
 }
