612d34967980031f7c7735e9eb3eafb3ef7ae506
Thomas <thomashj93@gmail.com>
2023-09-04 07:32:28 +0200

    add Persistence implementation duplicate v2

--- Katas/KatasOne.cs
+++ Katas/KatasOne.cs
@@ -820,11 +820,31 @@ public class KatasOne
         return counter;
     }
 
+    
+    
+    
+    // 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+    // 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+    // 4 --> 0 (because 4 is already a one-digit number)
     public static int Persistence2(long n)
     {
+        var parsedNumber = n.ToString();
+        var counter = 0;
+        var sum = n;
 
-
-        throw new NotImplementedException();
+        while (sum > 9)
+        {
+            sum = parsedNumber
+                .Select(c => int.Parse(c.ToString()))
+                .Aggregate((i1, i2) =>
+                {
+                    counter++;
+                    return i1 * i2;
+                });
+            parsedNumber = sum.ToString();
+        }
+        
+        return counter;
     }
     
     
