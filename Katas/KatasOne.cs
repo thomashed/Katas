7209b33bee9771b5f751faedafe2ce8bb00df840
Thomas <thomashj93@gmail.com>
2023-09-05 07:23:50 +0200

    format

--- Katas/KatasOne.cs
+++ Katas/KatasOne.cs
@@ -820,10 +820,6 @@ public class KatasOne
         return counter;
     }
     
-    
-    // 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
-    // 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
-    // 4 --> 0 (because 4 is already a one-digit number)
     public static int Persistence2(long n)
     {
         var counter = 0;
